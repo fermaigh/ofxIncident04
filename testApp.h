bec39a0d82a0e53dd11406431bf73a0838457e28
Xiaoye Lin <koko222@msn.com>
2013-04-28 13:56:52 -0300

    Create testApp.h

---
+++ testApp.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBlur.h"
+#include "ofxThinkGear.h"
+#include "ofxTweenLite.h"
+
+class testApp : public ofBaseApp{
+
+  public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        ofVideoPlayer welcomescreen;
+        ofVideoPlayer gameplay;
+        ofVideoPlayer gameover;
+    
+        vector<ofFbo*> fbos;
+    
+        ofxBlur blur;
+
+    
+        ofTrueTypeFont	alert;
+        ofImage transparency;
+        ofImage playing;
+        ofImage notplaying;
+    
+private:
+    
+    ofxThinkGear thinkGear;
+    void attentionListener(float &param);
+    
+    float atChangeTime;
+    float meChangeTime;
+    float distAw;
+    float prevAw;
+    float currAw;
+    float distMw;
+    float prevMw;
+    float currMw;
+    
+    ofTrueTypeFont font;
+    
+    float attention;
+    
+    float currBlur;
+    
+    bool welcome;
+    bool incident;
+    bool over;
+    bool danger;
+    bool extreme;
+    bool firstPlaySeq;
+    bool lastPlaySeq;
+    
+    int counter;
+    int threshold;
+    int openThreshold;
+    
+    float startTime;
+    float currentTime;
+    float totalTime;
+    bool flip;
+    
+   
+
+    
+};
